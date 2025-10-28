719985b50f0ee401249e9283b472ad0d51736ddf
Khaled Almahallawy <khaled.almahallawy@intel.com>
2025-10-28 14:10:20 -0700

    drm/i915: Add DP2 UHBR Test Pattern registers definition
    Add DP2_COMP_CTL and DP2_COMP_CUSTOM registers to support DP2 PHY CTS
    
    Bspec: 71418, 71418
    Cc: Jani Nikula <jani.nikula@intel.com>
    Cc: Imre Deak <imre.deak@intel.com>
    Cc: Lee Shawn C <shawn.c.lee@intel.com>
    Signed-off-by: Khaled Almahallawy <khaled.almahallawy@intel.com>

--- drivers/gpu/drm/i915/display/intel_display_regs.h
+++ drivers/gpu/drm/i915/display/intel_display_regs.h
@@ -2379,6 +2379,22 @@ enum skl_power_gate {
 #define _DDI_DP_COMP_PAT_B			0x615F4
 #define DDI_DP_COMP_PAT(pipe, i)		_MMIO(_PIPE(pipe, _DDI_DP_COMP_PAT_A, _DDI_DP_COMP_PAT_B) + (i) * 4)
 
+/* DDI DP2 Compliance Control */
+#define _DDI_DP2_COMP_CTL_A			0x60600
+#define _DDI_DP2_COMP_CTL_B			0x61600
+#define DDI_DP2_COMP_CTL(pipe)			_MMIO_PIPE(pipe, _DDI_DP2_COMP_CTL_A, _DDI_DP2_COMP_CTL_B)
+#define   DDI_DP2_COMP_CTL_ENABLE		(1 << 31)
+#define   DDI_DP2_COMP_CTL_RESET_PAT		(1 << 30)
+#define   DDI_DP2_COMP_CTL_PRBS7		(2 << 24)
+#define   DDI_DP2_COMP_CTL_PRBS9		(3 << 24)
+#define   DDI_DP2_COMP_CTL_PRBS11		(4 << 24)
+#define   DDI_DP2_COMP_CTL_PRBS15		(5 << 24)
+#define   DDI_DP2_COMP_CTL_PRBS23		(6 << 24)
+#define   DDI_DP2_COMP_CTL_PRBS31		(7 << 24)
+#define   DDI_DP2_COMP_CTL_CUSTOM_264		(8 << 24)
+#define   DDI_DP2_COMP_CTL_SQUARE		(9 << 24)
+#define   DDI_DP2_COMP_CTL_SQUARE_WAVE_NUMBER	(0xFF << 16)
+
 /* LPT PIXCLK_GATE */
 #define PIXCLK_GATE			_MMIO(0xC6020)
 #define  PIXCLK_GATE_UNGATE		(1 << 0)
