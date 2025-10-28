9dcd38682b2c962096d782f11398fb6ca6006ce9
Khaled Almahallawy <khaled.almahallawy@intel.com>
2025-10-28 16:10:07 -0700

    drm/i915/display: Handle DP2.1 for TEST_REQUEST DPCD
    Signed-off-by: Khaled Almahallawy <khaled.almahallawy@intel.com>

--- drivers/gpu/drm/i915/display/intel_dp_test.c
+++ drivers/gpu/drm/i915/display/intel_dp_test.c
@@ -408,6 +408,7 @@ void intel_dp_test_request(struct intel_dp *intel_dp)
 		response = intel_dp_autotest_edid(intel_dp);
 		break;
 	case DP_TEST_LINK_PHY_TEST_PATTERN:
+	case DP_TEST_LINK_PHY_TEST_PATTERN | DP_TEST_PHY_TEST_CHANNEL_CODING_TYPE:
 		drm_dbg_kms(display->drm, "PHY_PATTERN test requested\n");
 		response = intel_dp_autotest_phy_pattern(intel_dp);
 		break;
@@ -560,6 +561,7 @@ bool intel_dp_test_short_pulse(struct intel_dp *intel_dp)
 		intel_hpd_trigger_irq(dp_to_dig_port(intel_dp));
 		break;
 	case DP_TEST_LINK_PHY_TEST_PATTERN:
+	case DP_TEST_LINK_PHY_TEST_PATTERN | DP_TEST_PHY_TEST_CHANNEL_CODING_TYPE:
 		drm_dbg_kms(display->drm,
 			    "PHY test pattern Compliance Test requested\n");
 		intel_dp_test_phy(intel_dp);
